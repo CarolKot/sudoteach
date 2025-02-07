3af9113346c03376d1f4b83d95b9dfee68679807
CarolKot <kerolkot@ymdex.by>
2025-02-07 19:10:15 +0300

    используется для сопоставления значений

---
+++ match_case.py
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=Ql1tPKMqILA&list=PLV0FNhq3XMOJSj0TfDdHxgIKDKlTwSpmg&index=6
+# math case используется для сопоставления значений.
+
+command = input("Ведите команду ")
+
+match command:
+    case "start":
+        print("Запуск")
+    case "stop":
+        print("Остановка")
+    case _: # _: означает else
+        print("Неизвестная команда")
